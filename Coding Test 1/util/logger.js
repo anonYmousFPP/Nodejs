2880699f6cb5ea32b9b64cdf44b8fd423772bae5
abhay <abhaygaming14122001@gmail.com>
2025-04-22 12:38:13 +0530

    Merge remote changes

---
+++ Coding Test 1/util/logger.js
@@ -0,0 +1,38 @@
+import winston from 'winston';
+import morgan from 'morgan';
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/combined.log' }),
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      )
+    })
+  ]
+});
+
+const morganMiddleware = morgan(
+  function (tokens, req, res) {
+    return JSON.stringify({
+      method: tokens.method(req, res),
+      url: tokens.url(req, res),
+      status: tokens.status(req, res),
+      response_time: tokens['response-time'](req, res) + 'ms'
+    });
+  },
+  {
+    stream: {
+      write: (message) => logger.info(`HTTP Request: ${message}`)
+    }
+  }
+);
+
+export { logger, morganMiddleware };
